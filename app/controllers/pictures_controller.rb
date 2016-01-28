1bb6a758840e4c22248a3f7f212feec58cc5842c
ALEX SLYE <slyealex@live.com>
2016-01-28 17:34:07 -0500

    removed comments feature, rework in progress

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -40,7 +40,7 @@ class PicturesController < ApplicationController
 
   def show
     @picture = Picture.find(params[:id])
-    @comments = @photo.comment
+    # @comments = @photo.comment
   end
 
   private
